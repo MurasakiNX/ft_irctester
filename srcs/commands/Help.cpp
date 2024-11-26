8734efeb56fc841a3758a07fff2b8f39c9fdf9f6
MurasakiNX <murasakinx@Mura-PC.>
2024-11-26 05:32:14 +0100

    Finished tester

--- srcs/commands/Help.cpp
+++ srcs/commands/Help.cpp
@@ -10,8 +10,8 @@ std::string Help::execute(Client* client, std::vector<std::string> params) {
     print_colored("= [1]: /help: Prints the help message (The thing you are currently reading)                                                                                     =", CYAN);
     print_colored("= [2]: /send_as_privmsg <destination,key> <message>: Sends a PRIVMSG to the destination (If it's a channel, the key may be required)                            =", CYAN);
     print_colored("= [3]: /send_as_raw <message>: Sends a message to the server                                                                                                    =", CYAN);
-    print_colored("= [4]: /sendf_as_privmsg <destination,key> <file>: Sends a the content of a file as a PRIVMSG to the destination (If it's a channel, the key may be required)   =", CYAN);
-    print_colored("= [5]: /sendf_as_raw <file>: Sends a the content of a file as a message to the server                                                                           =", CYAN);
+    print_colored("= [4]: /sendf_as_privmsg <destination,key> <file>: Sends the content of a file as a PRIVMSG to the destination (If it's a channel, the key may be required)     =", CYAN);
+    print_colored("= [5]: /sendf_as_raw <file>: Sends the content of a file as a message to the server                                                                             =", CYAN);
     print_colored("= [6]: /toggle_raw: Toogle raw system (If it is enabled, the tester will send as raw your messages if it does not start with /)                                 =", CYAN);
     print_colored("=================================================================================================================================================================", CYAN);
     return "";
